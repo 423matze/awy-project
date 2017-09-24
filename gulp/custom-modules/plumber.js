5acb04d54619a229f95f416f5f702644f862359b
423matze <matze@423.ch>
2017-09-24 23:48:47 +0200

    trivis update

--- gulp/custom-modules/plumber.js
+++ gulp/custom-modules/plumber.js
@@ -3,12 +3,20 @@ var plumber = require('gulp-plumber');
 
 //
 function customPlumber(errTitle){
-    return plumber({
+    if (process.env.CI) {
+        return plumber({
+        errorHandler: function(err) {
+        throw Error(err.message);
+        }
+        });
+    } else {
+        return plumber({
         errorHandler: notify.onError({
             title: errTitle || "Error runnig Gulp MAL",
             message: "Error: <%= error.message %>"
         })
     });
+    };
 };
 
 module.exports = customPlumber;
